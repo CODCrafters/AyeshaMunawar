6ae35c33e16c82b6c779f169f523d92f48e14bbd
Ayesha Munawar <108673274+ayeshamunawar1@users.noreply.github.com>
2023-08-08 09:13:38 +0500

    First JAVASCRIPT CODE!
    That includes comments, variable declaration, constants, Primitive Types.

---
+++ index.js
@@ -0,0 +1,37 @@
+//this is my First Javasript Code
+
+// Variables
+console.log('Hello World');
+let a = 17;
+console.log(a);
+console.log(4+4);
+a=5;
+console.log(a);
+// Cannot be a reserved keyword
+//Should be meaningful
+//Cannot start with a number
+//Cannot contain a space or hyphen(-)
+// Are case-sensiive
+
+let firstname ='Ayesha' , lastname='Munawar';
+console.log(firstname , lastname);
+
+// Constants
+let Rate = 0.3;     //variable declaraion
+Rate = 1;
+console.log(Rate);
+ 
+const interestrate = 0.3;   //constant declaraion
+console.log(interestrate);
+
+// Primitive types
+let name = 'Ali'; //string literal
+let age  = 30; // Number Literal
+let isapproved = true;  // Boolean literal
+let fname = undefined; //undefined
+let selecedcolor = null;  //null
+// Dynamic typing
+typeof age;
+
+
+ 
